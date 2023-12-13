8831c67d3dafcf8abb8eb6a9ce68f04ca765b34d
Gabriel Rost <gabrielrost08@gmail.com>
2023-12-13 05:21:09 -0300

    Carrito de Compras

--- 4entradaysalida.js
+++ 4entradaysalida.js
@@ -1,199 +1,128 @@
-/*
-//VARIABLES-------------------------------------------//
-let nombre_usuario;
-let apellido_usuario;
-let edad_usuario;
-let botellas_cantidad;
-let precio_total;
-let precio_vodka_descuento;
-let stock_vodka=200
-//CONSTANTES------------------------------------------//
-const valor1=1;
-const mayor_edad=18;
-const precio_vodka=2450;
-//CÓDIGO----------------------------------------------//
-
-*/
-
-/*-----------DO-WHILE-----------------*/
-
-/*
-
-do {
-    nombre_usuario = prompt("Ingrese su nombre: ");
-    if (nombre_usuario === "") {
-        alert("Ingrese un nombre de usuario válido!")
-    }
-} while (nombre_usuario === "");
-
-do {
-    apellido_usuario = prompt("Ingrese su apellido: ");
-    if (apellido_usuario === "") {
-        alert("Ingrese un apellido válido!")
-    }
-} while (apellido_usuario === "");
-
-do {
-    edad_usuario = parseInt(prompt("Ingrese su edad: "));
-    if (edad_usuario <= 0 || edad_usuario > 99 || isNaN(edad_usuario)) {
-        alert("Ingrese una edad válida");
-    }
-} while (edad_usuario <= 0 || edad_usuario > 99 || isNaN(edad_usuario))
-
-
-    if (edad_usuario < mayor_edad) {
-        alert("Usted es menor de edad, no puede ingresar a la página!");
-    } 
-    else {
-        do {
-            botellas_cantidad = parseInt(prompt("Hola, " + nombre_usuario + " " + apellido_usuario + ", el precio de la botella de vodka es $2450, cuantas botellas desea comprar? \nSi compra más de 50 obtiene un 10% de descuento"));
-            //SI LA CANTIDAD DE BOTELLAS ES MENOR O IGUAL A 0//
-            if(botellas_cantidad<=0){
-                alert("La compra no se puede realizar si la cantidad de botellas es menor o igual a 0!");            
-            }
-            //SI LA CANTIDAD DE BOTELLAS ES MAYOR AL STOCK
-            else if (botellas_cantidad>stock_vodka){
-                    alert("No contamos con suficiente stock, la compra máxima no debe superar las " + stock_vodka + " botellas");                
-            }
-            //SI LA CANTIDAD DE BOTELLAS ES NULA//
-            else if(isNaN(botellas_cantidad)){
-                alert("Por favor ingrese un número válido!")
-            }
-        }
-        //HACER EL BUCLE SI BOTELLAS ES MENOR A 0, MAYOR A 200 O NULA// 
-        while (botellas_cantidad <= 0 || botellas_cantidad>200 || isNaN(botellas_cantidad)) {
-        }
-        //SI EL BUCLE TERMINÓ CALCULAR SI APLICA EL DESCUENTO    
-        if (botellas_cantidad >= 50) {
-            //SE CALCULA DESCUENTO DEL 10% PARA DECIR CUANTO AHORRÓ//    
-            precio_vodka_descuento = (botellas_cantidad * precio_vodka * 0.1);
-            //PRECIO FINAL DEL PRODUCTO, CON EL DESCUENTO//               
-            precio_total = (botellas_cantidad * precio_vodka * 0.9);
-            alert("El precio total es de $" + precio_total + " ARS \nUsted obtuvo un descuento de $" + precio_vodka_descuento + " ARS");            
-        }
-        else {
-            //SI COMPRA MENOS DE 50U, NO HAY DESCUENTO//
-            precio_total = (botellas_cantidad * precio_vodka);
-            alert("El precio total es de $" + precio_total + " ARS");
-        }        
-    }
-
-*/
-
-/*--------------------FUNCIONES--------*/
-
-/*
-
-function carritoDeCompras(producto_carrito1,producto_carrito2){
-    let total_carrito;
-    total_carrito=producto_carrito1+producto_carrito2;
-    return total_carrito;
-}
-let producto_carrito1=+prompt("Ingrese un numero");
-let producto_carrito2=+prompt("ingrese el otro numero");
-
-let result=carritoDeCompras(producto_carrito1,producto_carrito2);
-
-alert(`el numero es ${result}`);
-
-*/
-
-let precio_total;
-let precio_vodka_descuento;
-let stock_vodka=200
-
-
-const mayor_edad=18;
-
-
-/*-------OBJETOS-------------*/
-
-class Producto {
-    nombre;
-    precio;
-
-    constructor(nombre, precio) {
-        this.nombre = nombre;
-        this.precio = precio;
-    }
-}
 
 const productos = [
-    new Producto("Smirnoff", 2600),
-    new Producto("Absolut", 3000),
-    new Producto("Skyy", 2600),
-    new Producto("New Style", 2000)
-];
+    /* ID 1-20 VODKA- 21-40 VINOS- 41-60 CERVEZA*/
+    { id: 1, marca: 'Smirnoff', tipo: 'Común', precio: 600, },
+    { id: 2, marca: 'Smirnoff', tipo: 'Raspberry', precio: 600 },
+    { id: 3, marca: 'Smirnoff', tipo: 'Citrus', precio: 600 },
 
-let nombre_usuario, apellido_usuario, edad_usuario, botellas_cantidad, productoSeleccionado;
+    { id: 4, marca: 'Absolut', tipo: 'Común', precio: 600 },
+    { id: 5, marca: 'Absolut', tipo: 'Raspberry', precio: 600 },
+    { id: 6, marca: 'Absolut', tipo: 'Pera', precio: 600 },
 
-function obtenerDatosUsuario() {
-    do {
-        nombre_usuario = prompt("Ingrese su nombre: ");
-        if (nombre_usuario === "") {
-            alert("Ingrese un nombre de usuario válido!")
-        }
-    } while (nombre_usuario === "");
+    { id: 7, marca: 'Skyy', tipo: 'Común', precio: 600 },
+    { id: 8, marca: 'Skyy', tipo: 'Raspberry', precio: 600 },
+    { id: 9, marca: 'Skyy', tipo: 'Maracuyá', precio: 600 },
 
-    do {
-        apellido_usuario = prompt("Ingrese su apellido: ");
-        if (apellido_usuario === "") {
-            alert("Ingrese un apellido válido!")
-        }
-    } while (apellido_usuario === "");
+    { id: 10, marca: 'New Style', tipo: 'Común', precio: 600 },
+    { id: 11, marca: 'New Style', tipo: 'Raspberry', precio: 600 },
+    { id: 12, marca: 'New Style', tipo: 'Citrus', precio: 600 },
 
-    do {
-        edad_usuario = parseInt(prompt("Ingrese su edad: "));
-        if (edad_usuario <= 0 || edad_usuario > 99 || isNaN(edad_usuario)) {
-            alert("Ingrese una edad válida");
-        }
-    } while (edad_usuario <= 0 || edad_usuario > 99 || isNaN(edad_usuario));
 
-    if (edad_usuario < mayor_edad) {
-        alert("Usted es menor de edad, no puede ingresar a la página!");
-        return false;
+    /*---------VINOS------*/
+
+    { id: 21, marca: 'Dadá', tipo: 'Común', precio: 600 },
+    { id: 22, marca: 'Dadá', tipo: 'Honey', precio: 600 },
+    { id: 23, marca: 'Dadá', tipo: 'Negra', precio: 600 },
+
+    { id: 24, marca: 'Álamos', tipo: 'Común', precio: 600 },
+    { id: 25, marca: 'Álamos', tipo: 'Blanco', precio: 600 },
+    { id: 26, marca: 'Álamos', tipo: 'Malbec', precio: 600 },
+
+    { id: 27, marca: 'Portillo', tipo: 'Común', precio: 600 },
+    { id: 28, marca: 'Portillo', tipo: 'Blanco', precio: 600 },
+    { id: 29, marca: 'Portillo', tipo: 'Rosé', precio: 600 },
+
+
+    /*--------CERVEZAS----------*/
+
+
+    { id: 41, marca: 'Brahma', tipo: 'Chopp', precio: 600 },
+    { id: 42, marca: 'Brahma', tipo: 'Dorada', precio: 600 },
+    { id: 43, marca: 'Brahma', tipo: 'Malzbier', precio: 600 },
+
+    { id: 44, marca: 'Patagonia', tipo: 'Large', precio: 600 },
+    { id: 45, marca: 'Patagonia', tipo: 'Hoppy Larger', precio: 600 },
+    { id: 46, marca: 'Patagonia', tipo: 'Sendero Sur', precio: 600 },
+
+
+    { id: 47, marca: 'Quilmes', tipo: 'Común', precio: 600 },
+    { id: 48, marca: 'Quilmes', tipo: 'Stout', precio: 600 },
+    { id: 49, marca: 'Quilmes', tipo: '0 Alcohol', precio: 600 },
+
+
+    { id: 50, marca: 'Andes', tipo: 'Rubia', precio: 600 },
+    { id: 51, marca: 'Andes', tipo: 'Roja', precio: 600 },
+    { id: 52, marca: 'Andes', tipo: 'Ipa', precio: 600 },
+
+
+    { id: 53, marca: 'Imperial', tipo: 'Larger', precio: 600 },
+    { id: 54, marca: 'Imperial', tipo: 'Golden', precio: 600 },
+    { id: 55, marca: 'Imperial', tipo: 'Cream Stout', precio: 600 },
+]
+
+let carritoCompras = [];
+
+let precioTotal = 0;
+
+function agregarAlCarrito(productoId) {
+    const productoSeleccionado = productos.find(producto => producto.id === productoId);
+    const productoEnCarrito = carritoCompras.find(item => item.id === productoSeleccionado.id);
+    if (productoEnCarrito) {
+        productoEnCarrito.cantidad += 1;
+    } else {
+        productoSeleccionado.cantidad = 1;
+        carritoCompras.push(productoSeleccionado);
     }
-    return true;
+
+    precioTotal += productoSeleccionado.precio;
+
+    mostrarCarritoEnHTML();
+    mostrarCarritoEnConsola();
 }
 
-function obtenerBotellasCantidad() {
-    do {
-        botellas_cantidad = parseInt(prompt(`Hola, ${nombre_usuario} ${apellido_usuario}, elija un vodka:\n1. Smirnoff\n2. Absolut\n3. Skyy\n4. New Style\n\nEl precio de la botella de vodka es $${productos[productoSeleccionado - 1].precio}, ¿cuántas botellas desea comprar?\nSi compra más de 50 obtiene un 10% de descuento`));
+function mostrarCarritoEnConsola() {
+    console.log('Contenido del carrito:');
+    carritoCompras.forEach(producto => {
+        console.log(`${producto.cantidad > 1 ? `${producto.cantidad} ` : ''}${producto.marca} ${producto.tipo} $${producto.precio}`);
+    });
+    console.log('Precio total:', precioTotal);
+}
 
-        if (botellas_cantidad <= 0) {
-            alert("La compra no se puede realizar si la cantidad de botellas es menor o igual a 0!");
-        } else if (botellas_cantidad > stock_vodka) {
-            alert(`No contamos con suficiente stock, la compra máxima no debe superar las ${stock_vodka} botellas`);
-        } else if (isNaN(botellas_cantidad)) {
-            alert("Por favor ingrese un número válido!")
+function mostrarCarritoEnHTML() {
+    const carritoContainer = document.getElementById('carrito-container');
+    carritoContainer.innerHTML = '<h3>Carrito de Compras</h3>';
+    carritoCompras.forEach(producto => {
+        carritoContainer.innerHTML += `
+            <p>
+                ${producto.cantidad > 1 ? `${producto.cantidad} ` : ''}${producto.marca} ${producto.tipo} $${producto.precio}
+            </p>
+        `;
+    });
+
+    carritoContainer.innerHTML += `<hr>`;
+    carritoContainer.innerHTML += `<p>Total: $${precioTotal}</p>`;
+}
+
+function agregarEventos() {
+    for (let i = 0; i < productos.length; i++) {
+        const productoId = productos[i].id;
+        const botonProducto = document.getElementById(`producto-${productoId}`);
+
+        if (botonProducto) {
+            botonProducto.addEventListener('click', () => agregarAlCarrito(productoId));
         }
-    } while (botellas_cantidad <= 0 || botellas_cantidad > 200 || isNaN(botellas_cantidad));
-
-    return botellas_cantidad;
-}
-
-function calcularPrecioTotal(producto, cantidad) {
-    const precioUnitario = producto.precio;
-    const descuento = (cantidad >= 50) ? precioUnitario * cantidad * 0.1 : 0;
-    const precioTotal = precioUnitario * cantidad - descuento;
-
-    return { precioTotal, descuento };
-}
-
-function realizarCompra() {
-    const usuarioValido = obtenerDatosUsuario();
-
-    if (!usuarioValido) {
-        return;
     }
 
-    productoSeleccionado = parseInt(prompt("Elija un vodka:\n1. Smirnoff\n2. Absolut\n3. Skyy\n4. New Style"));
-    const cantidad = obtenerBotellasCantidad();
-
-    const { precioTotal, descuento } = calcularPrecioTotal(productos[productoSeleccionado - 1], cantidad);
-
-    alert(`El precio total es de $${precioTotal} ARS\nUsted obtuvo un descuento de $${descuento} ARS`);
+    const carritoComprasBtn = document.querySelector('.carrito-compras a');
+    carritoComprasBtn.addEventListener('click', function (event) {
+        event.preventDefault(); 
+        toggleCarrito();
+    });
 }
 
-// realizar la compra//
-realizarCompra();
+function toggleCarrito() {
+    const carritoContainer = document.getElementById('carrito-container');
+    carritoContainer.style.display = (carritoContainer.style.display === 'none' || carritoContainer.style.display === '') ? 'block' : 'none';
+}
+
+agregarEventos();
